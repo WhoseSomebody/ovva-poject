4e22aede9ba9a74437ed1204e43996297c80e285
SerG-Y <serg.yaschuk@gmail.com>
2017-02-22 22:20:15 +0200

    added vkapi

---
+++ libs/vkapiHelper.js
@@ -0,0 +1,84 @@
+const request = require('request');
+const fs    = require('fs');
+const path = require('path');
+
+function postScheduleOnWall() {
+    return new Promise((resolve, reject) => {
+        const ACCESS_TOKEN = "24be8511a3d7b531f2a3252fe28ac1407e976aed8b14d94b88ff1a0840050294c52ec8503abd57c82f1d6"; //"a7c1c20183f2c5f780fa06299c94127f2920296fec338699b16540366ffc86a2021dd49a2f05cd1b3ef66";
+        const GROUP_ID = "140964630";
+        //"140564762";
+        const message = "Schedule";
+        const API_URL = "https://api.vk.com/method";
+
+        let getUploadUrl = new Promise((resolve, reject) => {
+            const requestURL = `${API_URL}/photos.getWallUploadServer?group_id=${GROUP_ID}&access_token=${ACCESS_TOKEN}&v=5.62`;
+            request(requestURL, (error, response, body) => {
+                const data = JSON.parse(body);
+                if(error || response.statusCode != 200 || data.error)
+                    reject({err: data.error});
+                else {
+                    resolve(data.response)
+                }
+            })
+        })
+
+        let uploadImage = (res, resolve, reject) => {
+            let req = request.post(res.upload_url, (error, response, body) => {
+                const data = JSON.parse(body);
+                if(error || response.statusCode != 200 || data.error)
+                    reject({err: data.error});
+                else {
+                    resolve(data)
+                }
+            })
+
+            let form = req.form();
+            form.append('photo', fs.createReadStream(path.join(__dirname, '../public/out.png')), {filename: 'out.png'});
+        }
+
+        let savePhoto = (res, resolve, reject) => {
+            const requestURL = `${API_URL}/photos.saveWallPhoto?group_id=${GROUP_ID}&photo=${res.photo}&server=${res.server}&hash=${res.hash}&access_token=${ACCESS_TOKEN}&v=5.62`;
+
+            request(requestURL, (error, response, body) => {
+                const data = JSON.parse(body);
+                if(error || response.statusCode != 200 || data.error)
+                    reject({err: data.error});
+                else {
+                    resolve(data.response[0])
+                }
+            })
+        }
+
+        let postToWall = (res, resolve, reject) => {
+            const requestURL = `${API_URL}/wall.post?owner_id=-${GROUP_ID}&friends_only=0&from_group=1&message=Schedule&attachments=photo${res.owner_id}_${res.id}&access_token=${ACCESS_TOKEN}&v=5.62`;
+
+            request(requestURL, (error, response, body) => {
+                const data = JSON.parse(body);
+                if(error || response.statusCode != 200 || data.error)
+                    reject({err: data.error});
+                else {
+                    resolve(data)
+                }
+            })
+        }
+
+        getUploadUrl
+            .then(res => {
+                return new Promise(uploadImage.bind(null, res));
+            })
+            .then(res => {
+                return new Promise(savePhoto.bind(null, res));
+            })
+            .then(res => {
+                return new Promise(postToWall.bind(null, res));
+            })
+            .then(res => {
+                resolve(res);
+            })
+            .catch(err => {
+                reject(err);
+            })
+        })
+}
+
+module.exports.postScheduleOnWall = postScheduleOnWall;
